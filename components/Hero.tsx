f09e60b1c19f623b08d02da868cf1d672b505b72
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-07-24 21:41:01 +0530

    Add Hero Section

---
+++ components/Hero.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TypewriterEffectSmooth } from './ui/TypewriterEffect';
+import MagicButton from './ui/MagicButton';
+import { BsPersonWorkspace } from 'react-icons/bs';
+
+const Hero = () => {
+
+    const words = [
+        {
+          text: "Hi,",
+        },
+        {
+          text: "I'm",
+        },
+        {
+          text: "Kaushik",
+          className: "text-purple dark:text-purple"
+        },
+        {
+          text: "Manivannan.",
+          className: "text-purple dark:text-purple"
+        },
+    ];
+
+    return (
+        <div className="pb-20 pt-36">
+            <div>
+                <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+                <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
+                <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
+            </div>
+            <div>
+            <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+                </div>
+            </div>
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    {/* <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                        Dynamic Web Magic with Next.js
+                    </h2> */}
+
+                    <TypewriterEffectSmooth 
+                        words={words}
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                    />
+
+                    <p className="text-center md:tracking-wider text-sm md:text-xl lg:text-2xl mb-5 md:mb-0">
+                        A Full-Stack AI Software Engineer based in Boston, MA.
+                    </p>
+
+                    <a href="#about">
+                        <MagicButton
+                            title="View my Projects"
+                            icon={<BsPersonWorkspace />}
+                            position="right"
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
